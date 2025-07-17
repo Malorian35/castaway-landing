d1c59fc45d6ef6593968b1b444886b8f444cc4f1
Malorian35 <malorian.3516.arms@gmail.com>
2025-07-17 12:36:30 +0300

    Добавлен LatestEpisodes

---
+++ src/components/Latestepisodes/Latestepisodes.tsx
@@ -0,0 +1,24 @@
+import PrimaryButton from "../ui/Button/Button";
+import sectionStyles from "./Latestepisodes.module.css";
+import { podcastEpisodes } from "../../const/const";
+import EpisodeCard from "../ui/EpisodeItem/EpisodeItem";
+
+const Latestepisodes = () => {
+  return (
+    <section className={sectionStyles.episodesSection} id="episodes">
+      <header className={sectionStyles.header}>
+        <h2 className={sectionStyles.sectionTitle}>Latest episodes</h2>
+        <div className={sectionStyles.buttonContainer}>
+          <PrimaryButton text={"View all episodes"} />
+        </div>
+      </header>
+      <div className={sectionStyles.episodesList}>
+        {podcastEpisodes.map((episode) => (
+          <EpisodeCard key={episode.id} {...episode} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Latestepisodes;
