a639b1d447d5b4308339e15d2d642927a4f6e61e
Malorian35 <malorian.3516.arms@gmail.com>
2025-07-17 13:12:41 +0300

    Добавлен Reviews

---
+++ src/components/Reviews/Reviews.tsx
@@ -0,0 +1,22 @@
+import { testimonials } from "../../const/const";
+import TestimonialCard from "../Reviewitem/Reviewitem";
+import sectionStyles from "./Reviews.module.css";
+
+const TestimonialsSection = () => {
+  return (
+    <section className={sectionStyles.testimonials}>
+      <div className={sectionStyles.testimonialsGrid}>
+        {testimonials.map((testimonial) => (
+          <TestimonialCard
+            key={testimonial.id}
+            title={testimonial.feedback}
+            name={testimonial.reviewer}
+            starCount={testimonial.rating}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TestimonialsSection;
